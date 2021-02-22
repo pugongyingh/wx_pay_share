984d09846aaf238cba3d5e9cfc287422378ca1b8
pugongyingh <pgyhh@sina.cn>
2021-02-22 10:23:43 +0800

    Add files via upload

---
+++ wx_pay_server/config.js
@@ -0,0 +1,27 @@
+/**
+ * 相关配置参数
+ */
+module.exports = {
+	// 商户号
+	mch: {
+		mch_id: '1605227372',
+		key: 'qqqqqqqqwwwwwwwweeeeeeeerrrrrrrr'
+	},
+	// 微信公众号
+	wx: {
+		configName: '公众号',
+		appId: 'wx1763dce290fb74d8',
+		appSecret: '474c278e65bf99c5897574d5128e1c81'
+	},
+	// 微信小程序
+	mp: {
+		configName: '小程序',
+		appId: 'wx3535f69a610cea9a',
+		appSecret: 'c192e646b53ceeafa565ac7fe9cab65a'
+	},
+	// 腾讯地图
+	qqMap: {
+		configName: '腾讯地图',
+		key: ''
+	}
+}
